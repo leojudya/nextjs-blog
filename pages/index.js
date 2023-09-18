4b97322972d3cebc0cf9d16af734afd499521825
Lin Zi Yuan <leojudya@gmail.com>
2023-09-18 15:47:20 +0800

    修改blog內容為作品

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,19 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>你好，我叫林子元。我是一名軟體工程師。你可以以<a href="mailto:leojudya@gmail.com">Email</a>的方式聯絡我</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>你好，我叫林子元。我是一名軟體工程師。</p>
+        <ul>
+          <li>
+            Email: <a href="mailto:leojudya@gmail.com">leojudya@gmail.com</a>
+          </li>
+          <li>
+            Tel: 0988-196-325
+          </li>
+        </ul>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Project</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
---
+++ posts/bump-estore.md
@@ -0,0 +1,18 @@
+---
+title: '攀岩用品電商網站 - Vue.js + .Net Core Web API 開發'
+date: '2023-09-02'
+---
+
+## Video Link
+
+[連結](https://www.youtube.com/watch?v=RaOVYK3qG3M&t=1386s)
+
+## 專案負責項目
+
+- 前端切版
+- 後端 API 架構設計
+- 商品相關資料蒐集 (參考現有購物網站資訊)
+
+- 第三方金流API界接 (綠界, LINE PAY)
+- Signal R 實作即時聊天室
+- 客服相關網頁開發 (常見問題, 聯繫我們等)
